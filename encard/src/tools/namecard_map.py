aef40dd55566ab58329aac1a41c188f4b9408a81
Deviant <112615715+DEViantUA@users.noreply.github.com>
2024-04-08 01:01:25 +0300

    Add files via upload

--- encard/src/tools/namecard_map.py
+++ encard/src/tools/namecard_map.py
@@ -20,14 +20,21 @@ class Get:
             return mapping[self.charter_id].get(name)
       else:
             data = await update_namecard(self.charter_id)
-            name_card_icon = data["data"]["other"]["nameCard"]["icon"]
-            name_card_icon_image = data["data"]["other"]["nameCard"]["icon"].replace("NameCardIcon","NameCardPic")
-            mapping[self.charter_id] = {
-               "id": data["data"]["other"]["nameCard"]["id"],
-               "icon": f"https://api.ambr.top/assets/UI/namecard/{name_card_icon}.png",
-               "image": f"https://api.ambr.top/assets/UI/namecard/{name_card_icon_image}_P.png"
-            }
-            
+            if data["data"]["other"] is None:
+               mapping[self.charter_id] = {
+                  "id": 210192,
+                  "icon": f"https://api.ambr.top/assets/UI/namecard/UI_NameCardPic_OST4_P.png",
+                  "image": f"https://api.ambr.top/assets/UI/namecard/UI_NameCardPic_OST4_P.png"
+               }
+            else:
+               name_card_icon = data["data"]["other"]["nameCard"]["icon"]
+               name_card_icon_image = data["data"]["other"]["nameCard"]["icon"].replace("NameCardIcon","NameCardPic")
+               mapping[self.charter_id] = {
+                  "id": data["data"]["other"]["nameCard"]["id"],
+                  "icon": f"https://api.ambr.top/assets/UI/namecard/{name_card_icon}.png",
+                  "image": f"https://api.ambr.top/assets/UI/namecard/{name_card_icon_image}_P.png"
+               }
+               
             with open(file_path, "w") as file:
                json.dump(mapping, file, indent=4)
 
